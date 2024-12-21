ddebde9373f96d886f70be7956fe2bec1aac5143
Ken Mitton <mittonk@alum.mit.edu>
2024-12-21 11:17:25 -0500

    Use shorter simulation runs to reduce flicker.

--- dice.cpp
+++ dice.cpp
@@ -32,6 +32,8 @@ void DICE::init_mem(uint16_t *pixel_buffer)
         video->pixel_buf = pixel_buffer;
    
         int game_idx = 0;  // Pong
+        // int game_idx = 7;  // Breakout
+
         GameDesc& g = game_list[game_idx];
         
         circuit = new Circuit(settings,
@@ -45,9 +47,12 @@ void DICE::run(void)
     // Run until the video frame is finished.
     if(circuit)
     {
+       // Run until we've got a full video frame, but not much more.
        while (!circuit->video.request_video_callback) {
-          circuit->run(2.5e-3 / Circuit::timescale); // Run 2.5 ms
+            // circuit->run(2.5e-3 / Circuit::timescale); // Run 2.5 ms
+          circuit->run(1.0e-3 / Circuit::timescale); // Run 1 ms
        }
+       // Headed out to video callback, clear request.
        circuit->video.request_video_callback = false;
     }
 }
